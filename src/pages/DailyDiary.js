ed1acf4c37fedf902f2040ee770e1bd52eff37bf
Keyan <keyantabor98@gmail.com>
2023-04-12 14:01:36 -0400

    added calendar to track workouts

--- src/pages/DailyDiary.js
+++ src/pages/DailyDiary.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import DailyDiaryForm from '/components/DailyDiaryForm';
 
 const DailyDiary = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleOpenModal = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
     return (
         <div>
             <h1>Daily Diary</h1>
-            <DailyDiaryForm />
+            <Button onClick={handleOpenModal}>Open Daily Diary Form</Button>
+            
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Daily Diary Form</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <DailyDiaryForm />
+                </Modal.Body>
+            </Modal>
         </div>
     );
 };
