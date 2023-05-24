6288d989895baf6a9ed8f8eb28f876e6846759a4
Keyan Tabor <keyantabor98@gmail.com>
2023-05-24 05:47:44 -0400

    fixed authentication and began styling

--- src/pages/DailyDiary.js
+++ src/pages/DailyDiary.js
@@ -14,11 +14,27 @@ const DailyDiary = () => {
     };
 
     return (
-        <div>
+<>
+<style type="text/css">
+        {`
+    .btn-purple {
+        backgound-color: transparent;
+        border-color: purple;
+      color: purple;
+    }
+    .btn-purple:hover { 
+        backgound-color: transparent;
+        border-color: purple;
+      color: purple;
+    }
+    `}
+      </style>
+
+        <div className=' bg-black text-white h-screen w-screen flex flex-col justify-center items-center'>
             <h1>Daily Diary</h1>
-            <Button onClick={handleOpenModal}>Open Daily Diary Form</Button>
+            <Button variant='purple' onClick={handleOpenModal}>Open Daily Diary Form</Button>
             
-            <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal centered show={showModal} onHide={handleCloseModal}>
                 <Modal.Header closeButton>
                     <Modal.Title>Daily Diary Form</Modal.Title>
                 </Modal.Header>
@@ -27,6 +43,7 @@ const DailyDiary = () => {
                 </Modal.Body>
             </Modal>
         </div>
+        </>
     );
 };
 
